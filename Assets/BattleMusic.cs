f98a7cbbc3380cf27338be3e12ae8fca10b782d7
yingleifang <55908629+yingleifang@users.noreply.github.com>
2022-10-12 12:53:12 -0500

    battleground music

--- Assets/BattleMusic.cs
+++ Assets/BattleMusic.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class BattleMusic : MonoBehaviour
 {
-    void Start()
+    void Awake()
     {
         AudioSource audioSource = GetComponent<AudioSource>();
         int index = (LevelManager.currentLevel + 1) / LevelManager.levelNums <= 0 ? 1 : (LevelManager.currentLevel + 1) / LevelManager.levelNums;
         string path = string.Format("BattleMusic/lvl{0}_battleMusic", index);
         audioSource.clip = Resources.Load<AudioClip>(path);
+        audioSource.Play();
     }
 }
