c5b7b554021713944188f75d60e5a735a793d91e
mrosa7 <72995985+mrosa7@users.noreply.github.com>
2022-11-27 20:50:23 -0600

    wip
    bug: switching lights based on current level, and turning off once boss level is reached. Order?

--- Assets/lightingManager.cs
+++ Assets/lightingManager.cs
@@ -1,13 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Rendering.Universal;
 
 public class lightingManager : MonoBehaviour
 {
     public static lightingManager instance;
     LevelManager levelManager;
-    int currentIndex;
+    GameObject lightGO;
+    int current;
     int totalLevels;
+    int bossLvls;
+
+    GameObject ray1;
+    GameObject ray2;
+    GameObject tilemapGlobal;
+    GameObject bgGlobal;
     private void Awake()
     {
         // turn off global light, bc u can't see shit if its off in the editor
@@ -33,11 +42,56 @@ public class lightingManager : MonoBehaviour
 
     }
 
-    //public static void changeLights()
-  //  {
-        // time to switch to boss lights
-    //    instance.gameObject.transform.GetChild(1).gameObject.SetActive(false);
-   //     instance.gameObject.transform.GetChild(2).gameObject.SetActive(true);   
-   // }
+    private void OnEnable()
+    {
+        Debug.Log("ENABLE RUNNING");
+        SceneManager.sceneLoaded += onSceneLoad;
+    }
+
+    void onSceneLoad(Scene scene, LoadSceneMode mode)
+    {
+        
+        current = LevelManager.currentLevel;
+        totalLevels = levelManager.totalLevels;
+        bossLvls = levelManager.BossLevels;
+
+        Debug.Log(current);
+
+        //change lights to match new bg
+        if (current > 2 && current < (totalLevels - bossLvls))
+        {
+            changeLights();
+        }
+        else if (current >= totalLevels - bossLvls)
+        {
+            Debug.Log("LIGHTS ARE SUPPOSE TO CHANGE?????????????????????????????????????????");
+            gameObject.transform.GetChild(1).gameObject.SetActive(false);
+        }
+
+
+
+    }
+    void changeLights()
+    {
+        lightGO = gameObject.transform.GetChild(1).gameObject;
+  
+        ray1 = lightGO.transform.GetChild(0).gameObject;
+        ray2 = lightGO.transform.GetChild(1).gameObject;
+        tilemapGlobal = lightGO.transform.GetChild(2).gameObject;
+        bgGlobal = lightGO.transform.GetChild(3).gameObject;
+
+        ray1.GetComponent<Light2D>().color = new Color32(214,248,239,255);
+        ray1.GetComponent<Light2D>().intensity = 0.31f;
+
+        ray2.GetComponent<Light2D>().color = new Color32(214, 248, 239, 255);
+        ray2.GetComponent<Light2D>().intensity = 0.27f;
+
+        tilemapGlobal.GetComponent<Light2D>().color = new Color32(177, 171, 190, 255);
+        tilemapGlobal.GetComponent<Light2D>().intensity = 1.22f;
+
+        tilemapGlobal.GetComponent<Light2D>().color = new Color32(164, 164, 164, 255);
+        tilemapGlobal.GetComponent<Light2D>().intensity = 1.2f;
+
+    }
 }
 
