414cdbd42285cd26a5d6a7c26e8f9515cd4e835d
Isaac Adams <ia81718@gmail.com>
2022-11-28 20:58:51 -0600

    Cancelling using ability now shows proper highlighting again Fix size of stats and passive tabs

--- Assets/Scripts/BattleManager.cs
+++ Assets/Scripts/BattleManager.cs
@@ -491,6 +491,9 @@ public class BattleManager : MonoBehaviour
             {
                 tileManager.HighlightPath(unit.GetTilesInAbilityRange(), Color.red);
             }
+        } else
+        {
+            SelectUnit(selectedUnit);
         }
     }
 
